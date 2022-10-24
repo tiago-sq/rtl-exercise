95959a021555c42b24b2a885f4eca65f81a38e51
Tiago Quadros <tiago.quadros@betrybe.com>
2022-10-24 14:29:31 -0300

    Envio do gabarito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,35 +1,68 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
+import generateNumber from '../helpers/generateNumber';
+
+jest.mock('../helpers/generateNumber');
 
 describe('Testes da aplicação', () => {
-  /*
-    Conforme os testes forem concluídos, remova o 'todo' dos testes para garantir que os
-    mesmos sejam executados.
-  */
+  test('1 - Conferir se o título "Par ou Ímpar" aparece na página.', () => {
+    renderWithRouter(<App />);
+    const title = screen.getByRole('heading', { level: 1, name: /Par ou Ímpar/i });
+    expect(title).toBeInTheDocument();
+  });
 
-  test.todo('1 - Conferir se o título "Par ou Ímpar" aparece na página.');
-  //Neste teste você deseja visualizar o elemento com o título.
-  //Não esqueça de conferir o código da aplicação para entender o contexto.
+  test('2 - Acessando a partir da Home a página About.', () => {    
+    const { history } = renderWithRouter(<App />);
+    const link = screen.getByRole('link', { name: /Sobre/i });
+    userEvent.click(link);
+    const title = screen.getByRole('heading', { level: 1, name: /Sobre o Par ou Ímpar/i });
+    expect(title).toBeInTheDocument();
+    expect(history.location.pathname).toBe('/about');
+  });
 
-  test.todo('2 - Acessando a partir da Home a página About.');
-  //Neste teste você deseja trabalhar com a navegação de rotas.
-  //Você pode utilizar os links da própria tela para isso.
+  test('3 - Disparar a mensagem de "Página não encontrada" ao acessar uma rota inválida.', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/not-found');
+    });
+    const title = screen.getByRole('heading', { level: 1, name: /Página não encontrada!/i });
+    expect(title).toBeInTheDocument();
+  });
 
-  test.todo('3 - Disparar a mensagem de "Página não encontrada" ao acessar uma rota inválida.');
-  //Neste teste você deseja acessar uma rota inválida.
+  test('4 - Validar a existência de 2 botões na tela.', () => {
+    renderWithRouter(<App />);
+    const btnPar = screen.getByTestId('par');
+    const btnImpar = screen.getByTestId('impar');
+    expect(btnPar).toBeInTheDocument();
+    expect(btnImpar).toBeInTheDocument();
+  });
 
-  test.todo('4 - Validar a existência de 2 botões na tela.');
-  //Neste teste você deseja validar a existência dos botões 'Par' ou 'Ìmpar'.
+  test('5 - Conferir se o jogo retorna a mensagem "Acertou" ao clicar na opção correta.', () => {
+    generateNumber.mockReturnValue(2);
+    renderWithRouter(<App />);
+    const btnPar = screen.getByTestId('par');
+    userEvent.click(btnPar);
+    const text = screen.getByText(/Acertou/i);
+    expect(text).toBeInTheDocument();    
+  });
 
-  test.todo('5 - Conferir se o jogo retorna a mensagem "Acertou" ao clicar na opção correta.');
-  //Talvez você prefira controlar as possibilidades neste teste.
+  test('6 - Conferir se o jogo retorna a mensagem "Errou" ao clicar na opção incorreta.', () => {
+    generateNumber.mockReturnValue(2);
+    renderWithRouter(<App />);
+    const btnPar = screen.getByTestId('impar');
+    userEvent.click(btnPar);
+    const text = screen.getByText(/Errou/i);
+    expect(text).toBeInTheDocument();  
+  });
 
-  test.todo('6 - Conferir se o jogo retorna a mensagem "Errou" ao clicar na opção incorreta.');
-  //Talvez você prefira controlar as possibilidades neste teste.
-
-  test.todo('7 - Conferir se um número é gerado na tela do jogo.');
-  //Talvez você prefira controlar as possibilidades neste teste.
+  test('7 - Conferir se um número é gerado na tela do jogo.', () => {
+    generateNumber.mockReturnValue(123);
+    renderWithRouter(<App />);
+    const number = screen.getByTestId('number');
+    expect(number).toHaveTextContent(123);
+    expect(number).toBeInTheDocument();
+  });
 });
